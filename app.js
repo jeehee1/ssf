7c03e05a7cc1002ff679a3a1df5ebcc6cccac993
jeehee1 <jeehee1w@gmail.com>
2023-06-25 17:45:33 +0900

    CREATE 스터디그룹 인덱스 페이지 - 그룹 타이틀 리스트 보여줌

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Studygroup = require('./models/studygroup')
+const Studygroup = require("./models/studygroup");
 
 // mongoose.set("strickQuery", true);
 mongoose.connect("mongodb://localhost:27017/ssf");
@@ -9,22 +9,21 @@ mongoose.connect("mongodb://localhost:27017/ssf");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected")
-})
+  console.log("Database connected");
+});
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/studygroups", async (req, res) => {
+  const studygroups = await Studygroup.find({});
+  res.render("studygroups/index", { studygroups });
 });
 
-app.get("/makestudygroup", async (req, res) => {
-    const group = new Studygroup({title: "second studygroup", subject: "IT", location: "서울", description: "테스트용 스터디그룹입니다"})
-    await group.save();
-    res.send(group);
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(3000, () => {
--- models/studygroup.js
+++ models/studygroup.js
@@ -7,8 +7,21 @@ const StudygroupSchema = new Schema({
   location: String,
   address: String,
   date: Date,
+  images: [String],
   description: String,
   capacity: Number,
+  participants: [String],
+  geometry: {
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+},
 });
 
 module.exports =  mongoose.model("Studygroup", StudygroupSchema);
