0aa85018ba9f5631ba3725de865e4f756b2073f0
jeehee1 <jeehee1w@gmail.com>
2023-07-24 09:11:41 +0900

    refactor: users 컨트롤러 생성 및 users 라우트 분리

---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+module.exports.renderRegisterForm = (req, res) => {
+  res.render("users/register");
+};
+
+module.exports.registerUser = async (req, res, next) => {
+  try {
+    const { email, username, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) return next();
+      req.flash("success", "회원가입이 완료되었습니다.");
+      res.redirect("/studygroups");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login");
+};
+
+module.exports.loginUser = (req, res) => {
+  req.flash("success", "성공적으로 로그인하였습니다");
+  const redirectUrl = res.locals.returnTo;
+  req.session.returnTo = null;
+  res.redirect(redirectUrl);
+};
+
+module.exports.logoutUser = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "로그아웃되었습니다");
+    res.redirect("/studygroups");
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -3,61 +3,28 @@ const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
+const users = require("../controllers/users");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router
+  .route("/register")
+  .get(users.renderRegisterForm)
+  .post(catchAsync(users.registerUser));
 
-router.post(
-  "/register",
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) return next();
-        req.flash("success", "회원가입이 완료되었습니다.");
-        res.redirect("/studygroups");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
-);
+router
+  .route("/login")
+  .get(users.renderLoginForm)
+  .post(
+    (req, res, next) => {
+      res.locals.returnTo = req.session.returnTo || "/studygroups";
+      next();
+    },
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.loginUser
+  );
 
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
-
-router.post(
-  "/login",
-  (req, res, next) => {
-    res.locals.returnTo = req.session.returnTo || "/studygroups";
-    next();
-  },
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  (req, res) => {
-    req.flash("success", "성공적으로 로그인하였습니다");
-    const redirectUrl = res.locals.returnTo;
-    req.session.returnTo = null;
-    res.redirect(redirectUrl);
-  }
-);
-
-router.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "로그아웃되었습니다");
-    res.redirect("/studygroups");
-  });
-});
+router.get("/logout", users.logoutUser);
 
 module.exports = router;
